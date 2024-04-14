1ec96c12ac15f7c927fced5af8614bf814b93b43
Vladislav Smirnov <v.smirnov@collegeschedule.ru>
2024-04-14 17:55:20 +0300

    chore

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  base: "/reflection/",
+  base: "/reflection",
   plugins: [react()],
   css: {
     postcss: {
