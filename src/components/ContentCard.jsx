bde50ee596338034a9a2392a466aa7057faf7d15
infeibal <a.fedorov@collegeschedule.ru>
2024-04-14 18:17:25 +0700

    fix delete button

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -8,7 +8,7 @@ export const ContentCard = ({
     <>
       <div className="relative group w-full h-min">
         <a
-          className="flex w-full h-min hover:bg-neutral-300 p-2 rounded-sm gap-2 z-2 items-center"
+          className="flex w-full h-min group-hover:bg-neutral-300 p-2 rounded-sm gap-2 z-2 items-center"
           target="_blank"
           href={siteLink}
         >
@@ -20,12 +20,12 @@ export const ContentCard = ({
               className="max-w-6 max-h-6 w-auto rounded-lg bg-center"
             />
           </div>
-          <div className="line-clamp-1 max-w-[250px]">{siteName}</div>
-          <div className="line-clamp-1 opacity-30 mr-10">{siteLink}</div>
+          <div className="line-clamp-1">{siteName}</div>
+          <div className="line-clamp-1 opacity-30">{siteLink}</div>
         </a>
 
         <div
-          className="flex absolute right-0 top-[8px] mr-4 z-20 items-end justify-end group-hover:opacity-100 opacity-0 duration-100 cursor-pointer hover:text-red-500"
+          className="flex absolute right-0 top-[8px] bg-neutral-300  px-4 z-20  items-end justify-end group-hover:opacity-100 opacity-0  cursor-pointer hover:text-red-500"
           onClick={() => {
             deleteContent(localStorageKey);
           }}
